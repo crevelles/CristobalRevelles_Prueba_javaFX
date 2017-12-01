1547bbc51b43672f2c6c79cda6709857954b4197
cristobal <revellestrabajo@hotmail.com>
2017-12-01 10:42:06 +0100

    1.0

---
+++ src/control/ControlFX.java
@@ -0,0 +1,41 @@
+package control;
+
+import java.awt.Button;
+import java.io.IOException;
+
+import control.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ControlFX {
+	
+	
+	@FXML
+	private Button abrirVentana;
+	
+	
+	@FXML
+	 public void abrirVentana() {
+	        try {
+	            FXMLLoader loader = new FXMLLoader();
+	            loader.setLocation(Main.class.getResource("../vista/FreeSolo.fxml"));
+	            AnchorPane page = (AnchorPane) loader.load();
+	            Stage sendStage = new Stage();
+	            sendStage.setTitle("Superheader");
+	            Scene scene = new Scene(page);
+	            sendStage.setScene(scene);
+	            sendStage.show();
+	           
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	 
+	 
+	
+	
+}
---
+++ src/control/Main.java
@@ -0,0 +1,29 @@
+package control;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("../vista/Login.fxml"));
+			Scene scene = new Scene(root,1000,600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Font.loadFont(getClass().getResourceAsStream("PoiretOne-Regular.ttf"), 14);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
